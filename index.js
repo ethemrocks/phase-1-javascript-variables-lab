50f49c01577cee5cfeff2e0751d612e198963a9d
Ethem Kacic <ethem3k@gmail.com>
2023-12-02 14:14:25 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution in this file!
+
+// Define the companyName as a constant
+const companyName = 'Scuber';
+
+// Define the mostProfitableNeighborhood using let
+let mostProfitableNeighborhood = 'Chelsea';
+
+// Define the companyCeo using let
+let companyCeo = 'Susan Smith';
+
+// Export the variables if needed (assuming you are using CommonJS modules)
+module.exports = {
+  companyName,
+  mostProfitableNeighborhood,
+  companyCeo
+};
